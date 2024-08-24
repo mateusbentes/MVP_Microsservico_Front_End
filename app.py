db43f0ff4259a68ede9f223e48395ea368e15364
mateusbentes <mateusbentes@gmail.com>
2024-08-24 14:42:17 -0300

    Substituição response para resposta

--- app.py
+++ app.py
@@ -11,31 +11,31 @@ API_URL = 'http://127.0.0.1:5001'
 @app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def notas():
     if request.method == 'GET':
-        response = requests.get(API_URL)
-        return jsonify(response.json())
+        resposta = requests.get(API_URL)
+        return jsonify(resposta.json())
 
     if request.method == 'POST':
         requisicao = request.json
-        response_local = requests.post(API_URL, json=requisicao)
-        response_notion = requests.post('http://127.0.0.1:5002/', json=requisicao)
+        resposta_local = requests.post(API_URL, json=requisicao)
+        resposta_notion = requests.post('http://127.0.0.1:5002/', json=requisicao)
         return jsonify({
-            "local_response": response_local.json(),
-            "notion_response": response_notion.json()
+            "resposta_local": resposta_local.json(),
+            "resposta_notion": resposta_notion.json()
         })
 
     if request.method == 'PUT':
         requisicao = request.json
-        response = requests.put(API_URL, json=requisicao)
-        return jsonify(response.json())
+        resposta = requests.put(API_URL, json=requisicao)
+        return jsonify(resposta.json())
 
     if request.method == 'DELETE':
         requisicao = request.json
-        response = requests.delete(API_URL, json=requisicao)
-        return jsonify(response.json())
+        resposta = requests.delete(API_URL, json=requisicao)
+        return jsonify(resposta.json())
     
     requisicao = request.get_json()
     # Envia a nota para o microsserviço do Notion
-    return jsonify(response.json())
+    return jsonify(resposta.json())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
